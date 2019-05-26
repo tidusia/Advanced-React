6b666ad1834ee8a3a16af73d9c520708e7d5081c
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2019-05-26 20:43:44 +0200

    Chap 8 - Implement NProgress

--- sick-fits/frontend/components/Header/index.js
+++ sick-fits/frontend/components/Header/index.js
@@ -1,5 +1,19 @@
-import Nav from "../Nav";
 import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
+import Nav from "../Nav";
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 import { Logo, StyledHeader } from "./styles";
 
